deab0453bcd770e84152ec4a5b819c9daa69e53b
Tabatha Roman <romantabatha17@gmail.com>
2020-09-12 19:17:26 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+# def prime?(n)
+#   for d in 2..(n - 1)
+#   if (n % d) == 0
+#     return false
+#   end
+#   end
+# true
+# end
+
+def prime?(n)
+  return false if n < 2
+
+  (2..n/2).none?{|i| n % i == 0}
+end
